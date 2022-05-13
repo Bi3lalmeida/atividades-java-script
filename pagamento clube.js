aa3489de1bf68fc9a5f6f7472fd253a1c08a7893
João Almeida <joao.almeida@justa.com.vc>
2022-05-13 16:42:08 -0300

    adicionadas telas html

--- pagamento clube.js
+++ pagamento clube.js
@@ -1,21 +1,24 @@
-function pagamento(numero){
+
+function pagamento(){
+var numero= document.getElementById("pagamentoid").value; 
+console.log(numero)
 
 	     switch (numero){
-	     	case 0:
+	     	case "0":
 	     	 console.log("Pagamento em Janeiro");
 	     	break;
-	     	case 1:
+	     	case "1":
 	     	 console.log("Pagamento em Fevereiro");
 	     	break;
-	     	case 2:
+	     	case "2":
 	     	 console.log("Pagamento em Março");
 	     	break;
-	     	case 3:
+	     	case "3":
 	     	 console.log("Pagamento em Abril");
 	        break;
 	     	default:
 	     	 console.log("Pagamento em Maio");
 	     	break;
 	     }
-return numero
+
 }
